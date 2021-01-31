3a4999c03c993dd70944c13ea4620123deb69165
ksator <ksator@arista.com>
2021-01-31 13:52:22 +0000

    m

--- netmiko/test.py
+++ netmiko/test.py
@@ -3,13 +3,15 @@ import os
 
 username = "arista"
 password = "arista"
-ip = "172.28.135.38"
+ip = "172.28.131.231"
 command = "show version"
 
 switch = {'device_type': 'arista_eos', 'host': ip, 'username': username, 'password': password, 'port': '22', 'timeout': 180}
 connection = ConnectHandler(**switch)
+connection.is_alive()
 cmd_output = connection.send_command(command)
 f=open(command + ".txt", "w")
 f.write(cmd_output)
 f.close
+connection.disconnect()
 
