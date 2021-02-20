9e72ea296aed73775831e86594efdb40d319563a
khelil sator <ksator@arista.com>
2021-02-20 20:37:42 +0100

    m

--- netmiko/collect_show_commands.py
+++ netmiko/collect_show_commands.py
@@ -1,17 +1,19 @@
 from netmiko import ConnectHandler
-import os 
+import os
 
 username = "arista"
 password = "arista"
 ip = "172.28.131.231"
-command = "show version"
+commands = ["show version", "show ip interface brief"]
 
 switch = {'device_type': 'arista_eos', 'host': ip, 'username': username, 'password': password, 'port': '22', 'timeout': 180}
 connection = ConnectHandler(**switch)
 connection.is_alive()
-cmd_output = connection.send_command(command)
-f=open(command + ".txt", "w")
-f.write(cmd_output)
-f.close
+for command in commands:
+    cmd_output = connection.send_command(command)
+    f=open(command + ".txt", "w")
+    f.write(cmd_output)
+    f.close
+
 connection.disconnect()
 
