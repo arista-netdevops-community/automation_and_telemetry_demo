5d0601da1e4effd8303b0073e9cdf3ee4bb7216b
ksator <ksator@arista.com>
2021-02-20 19:28:01 +0000

    m

---
+++ netmiko/configure.py
@@ -0,0 +1,16 @@
+
+from netmiko import ConnectHandler
+
+username = "arista"
+password = "arista"
+ip = "172.28.131.231"
+
+cfg_file = "config_changes.txt"
+
+switch = {'device_type': 'arista_eos', 'host': ip, 'username': username, 'password': password, 'port': '22', 'timeout': 180}
+connection = ConnectHandler(**switch)
+# connection.is_alive()
+connection.enable()
+connection.send_config_from_file(cfg_file)
+connection.disconnect()
+
